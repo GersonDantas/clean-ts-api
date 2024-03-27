676fb977c097ad4d6b847ac3f6826189e4b6248a
gersondantas <gerson.accari@hotmail.com>
2024-03-27 20:16:51 -0300

    test:  ensure SignUpController return an error if no name provider

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -15,5 +15,6 @@ describe('SignupController', () => {
     // when without property name expected error
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
