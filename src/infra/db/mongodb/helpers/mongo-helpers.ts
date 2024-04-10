3e92c02f905d3afaee5d38d347446b54695622a6
gersondantas <gerson.accari@hotmail.com>
2024-04-09 21:16:01 -0300

    refactor: move mongodb methods to a helper

---
+++ src/infra/db/mongodb/helpers/mongo-helpers.ts
@@ -0,0 +1,13 @@
+import { MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient | null, // para o javascript não conflitar com o typescript
+
+  async connect (url: string): Promise<void> {
+    this.client = await MongoClient.connect(url)
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client?.close()
+  }
+}
