af2815cee999038148afc6299e36d56429df9381
gersondantas <gerson.accari@hotmail.com>
2024-04-16 17:02:28 -0300

    refactor: remove database betwem tests

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -4,7 +4,7 @@ import { MongoHelper } from '../helpers/mongo-helpers'
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<boolean> {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     return result.insertedId !== null
   }
