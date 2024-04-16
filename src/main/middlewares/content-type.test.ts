0bd1d87961a185fdbc4127195fe333b5443b19c6
gersondantas <gerson.accari@hotmail.com>
2024-04-16 19:45:53 -0300

    test: ensure return xml content type when forced

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -11,4 +11,15 @@ describe('Content Type Middleware', () => {
       .get('/test_content_type')
       .expect('content-type', /json/) // prevent deferent write 'json'
   })
+
+  test('Should return xml content type when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
 })
